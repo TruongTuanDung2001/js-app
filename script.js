2660216c1de5dae6fa84ef1e56f31a703733386c
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-03-11 11:25:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+const aa = 10;
+let bb = 20;
+bb = aa + bb;
+console.log(bb);
+
+let result = document.getElementById("result");
+const submit = document.getElementById("submit");
+
+result.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();//submit form có thể reload trang nên cần preventDefault để ngăn chặn hành vi mặc định của form.
+        let value = result.value;
+        console.log(value);
+        result.value = "";
+    addArrayNumbers(Number(value));
+}});
+
+submit.addEventListener("click", function(){
+    let value = result.value;
+    console.log(value);
+    result.value = "";
+    addArrayNumbers(Number(value));
+});
+
+let arr = [1, 2, 3, 4, 5];
+
+function addArrayNumbers(numbers){
+    if(numbers === 0){
+        alert("Please enter a valid number");
+        return;
+    }
+    else if(numbers > 0 && numbers < 10){
+        arr.push(numbers);
+        console.log(arr);
+    }
+    else{
+        alert("Please enter a number between 1 and 9");
+    }
+}

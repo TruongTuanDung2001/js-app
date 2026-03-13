d600e218a4d8577cdd0a411d9e615c8e59496e5b
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-03-13 12:36:13 +0700

    DOM getElement ID - CLASS, query selector - all

--- script2.js
+++ script2.js
@@ -26,3 +26,23 @@ const sumAB = (a, b) => a + b;
 console.log(sumAB(5, 10));
 console.log(typeof(sumAB));
 
+//DOM
+let list = document.getElementById("list");
+console.log(list);
+console.log(list.innerHTML);
+console.log(list.textContent);
+
+let item = document.getElementsByClassName("item");
+console.log(typeof(item));
+console.log(item);
+console.log(item[0]);
+
+
+console.log("query");
+// let itemQuery = document.querySelector(".content"); //2 thẻ có class là content nhưng querySelector chỉ lấy thẻ đầu tiên 
+let itemQuery = document.querySelector("#list .content"); //cách này sẽ lấy thẻ có class là content nằm trong thẻ có id là list, vì vậy sẽ lấy được thẻ thứ 2 có class là content
+console.log(itemQuery);
+
+
+
+

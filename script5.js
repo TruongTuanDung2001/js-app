8c2e7051a2e8b2491137d875ead9e62d4ca47008
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-03-25 13:08:59 +0700

    Callback in javascript

--- script5.js
+++ script5.js
@@ -11,7 +11,29 @@ const user = (id, name, age, address) => {
     }
 }
 //Callback
+const color = (nameColor) =>  console.log("Color is " + nameColor);
+
+function getColor(cb){
+    let nameColor = "red";
+    cb(nameColor);
+}
+
+getColor(color);
+//Callback là một hàm được truyền vào một hàm khác như một đối số và được gọi sau khi một số sự kiện hoặc điều kiện xảy ra. Nó thường được sử dụng để xử lý các tác vụ bất đồng bộ, chẳng hạn như đọc dữ liệu từ một API hoặc xử lý sự kiện người dùng. Callback giúp đảm bảo rằng mã của bạn không bị chặn và có thể tiếp tục thực thi trong khi chờ đợi kết quả từ các tác vụ bất đồng bộ.
+
+//callback hell
+setTimeout(() => {
+    console.log("B1");
+    setTimeout(() => {
+        console.log("B2");
+        setTimeout(() => {
+            console.log("B3");
+        }, 1000);
+    }, 1000);
+}, 1000);
+//Lỏ, khó đọc, khó debug
 
 //Promise
 
 //Async/Await
+

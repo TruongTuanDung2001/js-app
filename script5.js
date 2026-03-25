313c8aa2bfce5c5cd16ae4084a192d40f4c7d243
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-03-25 12:41:59 +0700

    Arrow function in javascript

---
+++ script5.js
@@ -0,0 +1,17 @@
+//Arrow function
+function add(a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+const user = (id, name, age, address) => {
+    {
+        id, name, age, address
+    }
+}
+//Callback
+
+//Promise
+
+//Async/Await

de8944aad76f8d2c2ecf0b7cbaca6aef82ad57f3
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-04-03 11:05:50 +0700

    Dom delete user

--- exp/script4.js
+++ exp/script4.js
@@ -38,31 +38,104 @@ let userInput = document.getElementById("user");
 let list = document.getElementById("list");
 let btnAdd = document.getElementById("btn-add");
 let allUser = [];
+let btnDelete = document.getElementsByClassName("btn-delete");
 // list.innerHTML = `<li class="item-list">User 2</li>`
 
 function addUser(userInput){
-    if(userInput.value == 0){
+    if(userInput.value === ""){
         alert("No data, please check your input");
     }
     else{
         allUser.push(userInput.value);//allUser['user1', 'user2', ...]
-        list.innerHTML = "";
-        for(let i = 0; i < allUser.length; i++){
-            list.innerHTML += `<li class="item-list">${allUser[i]}</li> <br>`;
-        }
     }
     userInput.value = "";
     userInput.focus();
 }
 
+function render(){
+    list.innerHTML = "";
+    for(let i = 0; i < allUser.length; i++){
+        list.innerHTML += `<li class="item-list">${allUser[i]}
+            <button type="submit" id="deleteUser" class="btn-delete">❌</button>
+        </li>`;
+    }
+    console.log(allUser);
+}
+
 btnAdd.addEventListener("click", function(){
     addUser(userInput);
+    render();
 });
 
 userInput.addEventListener("keydown", function(e){
     if(e.key == "Enter"){
         addUser(userInput);
+        render();
     }
 });
 
 
+list.addEventListener('click', function(e){
+    if(e.target.classList.contains("btn-delete")){
+        console.log(e.target);
+        console.log(e.target.parentElement);
+        let li = e.target.parentElement; //lấy 1 thẻ li trong mà chứa cái button X vừa click
+        let index = Array.from(list.children).indexOf(li);// tìm vị trí thẻ li trong list thành con số trong mảng = index
+        let newArr = Array.from(list.children);
+        console.log(newArr);
+        /* 
+            0: li.item-list
+            1: li.item-list
+            2: li.item-list
+            3: li.item-list 
+        */
+        console.log(li);
+        console.log(index);
+        allUser.splice(index, 1);// xóa pt;
+        render();
+    }
+});
+
+/*
+🔢 5. Tìm vị trí trong list
+let index = Array.from(list.children).indexOf(li);
+
+Giải thích từng bước:
+
+👉 list.children
+Lấy tất cả <li> trong list
+Nhưng nó là HTMLCollection (không phải array)
+👉 Array.from(...)
+Biến nó thành array
+👉 .indexOf(li)
+Tìm vị trí của <li> vừa click
+
+👉 Ví dụ:
+
+["user1", "user2", "user3"]
+          ↑ index = 1
+*/
+
+/*LỖI BỊ LỆCH INDEX TRONG THẺ LI
+📦 Vì bạn đang render kiểu này:
+<li>User 1
+<button>❌</button>
+</li>
+<br>
+<li>User 2
+<button>❌</button>
+</li>
+<br>
+DƯ CÁI <BR> :)))))
+🧠 Nên list.children sẽ là:
+[
+  li,       // index 0
+  button,   // index 1
+  br,       // index 2
+  li,       // index 3
+  button,   // index 4
+  br,       // index 5
+  ...
+]
+
+*/
